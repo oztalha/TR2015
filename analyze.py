30665573acb495422e7297c962a6abd69377ae5b
Talha Oz <toz@gmu.edu>
2015-06-09 11:41:30 -0400

    11-15 merged as CSV

--- analyze.py
+++ analyze.py
@@ -14,18 +14,26 @@ import matplotlib.pyplot as plt
 # The dataset: http://www.luc.edu/faculty/gtezcur/files/TEPLWeb.xlsx
 
 df11 = pd.read_csv('data/TEPLWeb.csv',nrows=81,
-                usecols = ['PROV1NAME', 'AKPP11', 'CHPP11', 'MHPP11','BGMP11'])
+        usecols = ['PROV1NAME', 'VALID11','AKPP11', 'CHPP11', 'MHPP11','BGMP11'])
 df11 = df11.replace('AFYON','AFYONKARAHISAR')
 df11 = df11.replace('SANLI URFA','SANLIURFA')
 df11 = df11.rename(columns={'BGMP11':'HDPP11'})
-df11['OTHERS11'] = df11.apply(lambda x: 100 - sum(x[1:]),axis=1)
+df11['OTHERS11'] = df11.apply(lambda x: 100 - sum(x[2:]),axis=1)
 
 df15 = pd.read_csv('data/TR2015.csv')
-df15 = df15[~df15['province'].str.contains('\d')]
-df15['province'] = df15['province'].apply(lambda x: unidecode(x).upper())
+df15 = df15.rename(columns={'province':'PROVINCE'})
+df15 = df15[~df15['PROVINCE'].str.contains('\d')]
+df15['PROVINCE'] = df15['PROVINCE'].apply(lambda x: unidecode(x).upper())
+df15 = df15.drop('others',axis=1)
+df15['OTHERS'] = df15.apply(lambda x: 100 - sum(x[1:]),axis=1)
 
 # combine the both !
-df = df15.merge(df11,left_on='province',right_on='PROV1NAME')
+df = df15.merge(df11,left_on='PROVINCE',right_on='PROV1NAME')
+df = df.drop('PROV1NAME',axis=1)
+
+cols = [col for col in df.columns if col not in ['PROVINCE', 'VALID11']]
+df[cols] = df[cols]/100
+df.to_csv('data/TR_11_15.csv',index=False)
 
 # plot 2011-2015 comparison with self (for each party)
 plt.style.use('ggplot')
@@ -34,7 +42,7 @@ for p in parties:
     p11 = p + 'P11'
     lim = max(df[p].max(),df[p11].max()) + 5
     ax = df.plot(x=p11, y=p, kind='scatter', figsize=(15,15),xlim=(0,lim),ylim=(0,lim))
-    df.apply(lambda x: ax.annotate(x['province'], (x[p11],x[p]),
+    df.apply(lambda x: ax.annotate(x['PROVINCE'], (x[p11],x[p]),
                 xytext=(-30, 7), textcoords='offset points',fontsize=9), axis=1);
     ax.set_xlabel(p+" 2011 Vote Shares",fontsize=14);
     ax.set_ylabel(p+" 2015 Vote Shares",fontsize=14);
